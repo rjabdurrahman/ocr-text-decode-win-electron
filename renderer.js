6cba9e7b5ac5b87223c0036c6dd63e57f4e82b38
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-11-19 01:36:18 +0600

    Added: loader

--- renderer.js
+++ renderer.js
@@ -9,15 +9,17 @@ function eventBinder(selector, action, f) {
     $se(selector).addEventListener(action, f);
 }
 
+const loader = $('#loader');
+loader.hide();
 eventBinder('#convert', 'click', function() {
+    loader.show();
     let imgPath = $se('#img').files[0].path;
     exec(`tesseract ${imgPath} stdout`, (error, stdout) => {
         if (error) {
           console.error(`exec error: ${error}`);
           return;
         }
+        loader.hide();
         $se('#txt').value = stdout;
       });
 })
-
-console.log($('#txt'))
