f26491fac49b3e88a42576cc32124d52a4217b3c
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-11-19 00:59:11 +0600

    Added: Reloader and deisgn

--- main.js
+++ main.js
@@ -41,3 +41,6 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
